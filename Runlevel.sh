7e97eb4be0fca7f08b15589a46b92dbabb4cc57d
Joval14 <jotaval@outlook.com>
2018-02-27 21:58:37 +0000

    Runlevel.sh

---
+++ Runlevel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#This command show your actual runlevel
+
+echo 
+sudo runlevel
+echo
+
+#THis command show your level, date and time
+
+echo
+who -r
+echo
+
+#This command reboot your system
+
+echo
+sudo shutdown +30 -h
+
+echo "Save all your files now"
+
+#Thanks
+
